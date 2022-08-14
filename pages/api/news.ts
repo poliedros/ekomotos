3dc98f220f0709f24fa17ff028bc160d0a6e5769
Carlos <carloshlzansavio@gmail.com>
2022-08-14 01:52:47 +0100

    create next project

---
+++ pages/api/news.ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export type NewsDto = {
+  _id: string;
+  headline: string;
+  publishedAt: Date;
+  by: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<NewsDto[]>
+) {}
