ca97197e9b4b3dc17b473953588db2ab964b28ed
Carlos <carloshlzansavio@gmail.com>
2022-08-14 02:27:24 +0100

    feat: add form

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,78 @@ const Home: NextPage = () => {
   return (
     <div className="container mx-auto">
       <Head>
-        <title>Czar News</title>
+        <title>Ekomotos</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Ekomotos Oficina" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>hi</div>
+
+      <Form />
     </div>
   );
 };
 
 export default Home;
+
+function Form() {
+  return (
+    <div>
+      <div className="mb-6">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+          Nome
+        </label>
+        <input
+          type="text"
+          id="default-input"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-6">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+          Modelo
+        </label>
+        <input
+          type="text"
+          id="default-input"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-6">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+          Ano
+        </label>
+        <input
+          type="text"
+          id="default-input"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-6">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+          Serviço
+        </label>
+        <input
+          type="text"
+          id="default-input"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-6">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+          Descreva o servico
+        </label>
+        <input
+          type="text"
+          id="default-input"
+          className="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => alert("Email sent")}
+      >
+        Enviar
+      </button>
+    </div>
+  );
+}
