992c5e72b48ee8d5eadbe61d622f95b24699644f
Carlos <carloshlzansavio@gmail.com>
2022-08-14 13:58:52 +0100

    feat: add heading and footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,17 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <h1 className="font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600">
+        Ekomotos
+      </h1>
+
+      <h3 className="font-medium leading-tight text-3xl mt-0 mb-2 text-stone-600">
+        Agende conosco
+      </h3>
+
       <Form />
+
+      <Footer />
     </div>
   );
 };
@@ -33,6 +43,16 @@ function Form() {
       </div>
       <div className="mb-6">
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+          Data
+        </label>
+        <input
+          type="date"
+          id="default-input"
+          className="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-6">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
           Modelo
         </label>
         <input
@@ -46,7 +66,7 @@ function Form() {
           Ano
         </label>
         <input
-          type="text"
+          type="number"
           id="default-input"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
@@ -63,20 +83,41 @@ function Form() {
       </div>
       <div className="mb-6">
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-          Descreva o servico
+          Descreva o serviço
         </label>
         <input
           type="text"
           id="default-input"
-          className="block p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="block p-8 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
       </div>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => alert("Email sent")}
+        onClick={() => alert("Serviço agendado")}
       >
         Enviar
       </button>
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="p-4 bg-white rounded-lg md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © 2022{" "}
+        <a href="https://czar.dev" className="hover:underline">
+          Czar+™
+        </a>
+        . Todos os direitos reservados.
+      </span>
+      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+          <a href="https://czar.dev" className="hover:underline">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </footer>
   );
 }
